ee730cfdde5a0620bac4e21eaeb340b963520c68
M B Ashish <mbashishoo7@gmail.com>
2025-02-26 19:45:14 +0530

    Initial Commit

---
+++ src/test/java/com/iiitb/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiitb.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
